87512ccee2476ac1a1a1d776821257e666371854
Bodhian1999 <126016003+Bodhian1999@users.noreply.github.com>
2023-03-09 15:05:53 +0100

    Update 1_Test_Scores_Comparison.py

--- 1_Test_Scores_Comparison.py
+++ 1_Test_Scores_Comparison.py
@@ -4,14 +4,14 @@
 # In[40]:
 
 
-import kaggle
+#import kaggle
 import pandas as pd
 import numpy as np
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
-import seaborn as sns
-import matplotlib.pyplot as plt
+#import seaborn as sns
+#import matplotlib.pyplot as plt
 
 
 # In[41]:
